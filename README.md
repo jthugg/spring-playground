3ba0276fe5276c18cca63bd2891e77484e44f67a
Hi. I'm Neo <jthug@naver.com>
2024-08-15 11:11:04 +0900

    init: initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🚀 Embedded Debezium Example
+
+> This section explains how to use Debezium in embedded mode.
+
+
---
+++ module-application/src/main/java/com/github/jthugg/spring/playground/embedded/debezium/Application.java
@@ -0,0 +1,13 @@
+package com.github.jthugg.spring.playground.embedded.debezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
